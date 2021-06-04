9f9f41a51364f7a847d52e51e2265659dad481b9
akharkhalis <anton.harhalis@gmail.com>
2021-06-04 09:16:23 +0300

    added comments

--- hw_1.py
+++ hw_1.py
@@ -1,6 +1,6 @@
 stars = {"Alnilam": 27000, "Spica": 22000, "Sun": 6000, "Artur": 4000, "Betelgeuse": 3000}
 
-
+# todo: try to use as a key star name from the stars list, it's a more convenient approach
 def get_key(val):
     for key, value in stars.items():
         if val == value & val == 22000:
@@ -16,8 +16,17 @@ def get_key(val):
     return 'Currently, it is undefined star'
 
 
+# todo: to avoide code duplicating you can use for loop for all items in the list
+'''
+# for item in list:
+#     your_fucntion(item)
+'''
 get_key(27000)
 get_key(22000)
 get_key(6000)
 get_key(4000)
 get_key(3000)
+
+
+# todo: read about PEP8 -> https://www.python.org/dev/peps/pep-0008/ and write your code 
+# accordingly to it, code will be more readeble and easy to understand
